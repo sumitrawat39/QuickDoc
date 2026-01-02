246c66a1d9f4028fe535b76169337d115c407d35
sumitrawat39 <nirnay777@gmail.com>
2026-01-02 12:19:02 +0530

    Add Doctor components

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -19,7 +19,7 @@ function SpecialityMenu() {
             <Link onClick={()=>scrollTo(0,0)}
               className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500"
               key={index}
-              to={`/doctors/${item.speciality}`}
+              to={`/doctors/${item.speciality}`}                                                                                                                                                              
             >
               <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
               <p>{item.speciality}</p>
