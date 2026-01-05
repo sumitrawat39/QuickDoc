7f986092f87b6c274852c6432ffea9fa771d9b44
sumitrawat39 <nirnay777@gmail.com>
2026-01-05 18:47:39 +0530

    add TopDoctors compo and  fix some bug in TopDoc comp

--- src/components/RelatedDoctors.jsx
+++ src/components/RelatedDoctors.jsx
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
-// import { doctors } from "../assets/assets";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useState , useEffect} from "react";
 import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
-function TopDoctors() {
-  const navigate = useNavigate();
+function RelatedDoctors({ speciality, docId }) {
   const { doctors } = useContext(AppContext);
+  const [relDoc, setRelDocs] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter(
+        (doc) => doc.speciality === speciality && doc._id !== docId
+      );
+      setRelDocs(doctorsData);
+    }
+  }, [doctors, speciality, docId]);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
@@ -14,12 +23,9 @@ function TopDoctors() {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid  grid-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {relDoc.slice(0, 5).map((item, index) => (
           <div
-            onClick={() => {
-              navigate(`/appointment/${item._id}`);
-              scrollTo(0, 0);
-            }}
+            onClick={() =>{ navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
@@ -48,4 +54,4 @@ function TopDoctors() {
   );
 }
 
-export default TopDoctors;
+export default RelatedDoctors;
