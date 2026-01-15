9c1ea6a567f47be8392d5c727d35d5e094ed39f2
sumitrawat39 <nirnay777@gmail.com>
2026-01-15 22:30:09 +0530

    admin and doctor routes

---
+++ controllers/admin.controller.js
@@ -0,0 +1,19 @@
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      fees,
+      about,
+      experience,
+      address,
+    } = req.body;
+  } catch (error) {
+    
+  }
+};
+
+export { addDoctor };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/admin.routes.js";
 
 // app config
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 //API endpoints
+app.use("/api/admin", adminRouter);
+
 app.get("/", (req, res) => {
   res.send("API working");
 });
